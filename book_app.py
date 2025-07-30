5e52e8dffd373a86583305036e24efc5b09e40c9
Mehmet Ali KAZOGLU <mehmetalikazogluu@gmail.com>
2025-07-30 17:36:23 +0300

    Kitap güncelleme kısmında resim ekleme seçeneğindeki güncelleme hatası giderildi

--- book_app.py
+++ book_app.py
@@ -1,3 +1,4 @@
+import uuid
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
